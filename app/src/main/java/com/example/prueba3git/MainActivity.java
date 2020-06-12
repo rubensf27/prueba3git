77f6000eb2fcebd7ee8ef977c0f2c06b17ada3fe
Ruben <rubenpa.89@gmail.com>
2020-06-12 22:54:37 +0200

    commit change comment develop

--- app/src/main/java/com/example/prueba3git/MainActivity.java
+++ app/src/main/java/com/example/prueba3git/MainActivity.java
@@ -3,12 +3,50 @@ package com.example.prueba3git;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-// prueba subida 2
+public class MainActivity extends AppCompatActivity  {
+
+    Button btnSumar;
+    EditText edit1, edit2;
+    TextView lbResultado;
+
+// prueba subida 2 - develop
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSumar = (Button) findViewById(R.id.btnSumar);
+        edit1 = (EditText) findViewById(R.id.edit1);
+        edit2 = (EditText) findViewById(R.id.edit2);
+        lbResultado = (TextView) findViewById(R.id.lbResultado);
+
+        btnSumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double valor1= 0d;
+                double valor2 = 0d;
+                try{
+                    valor1 = Double.valueOf(edit1.getText().toString());
+                    valor2 = Double.valueOf(edit2.getText().toString());
+                }catch(NumberFormatException e){
+
+                }
+                double resultado = valor1 + valor2;
+                lbResultado.setText(String.valueOf(resultado));
+
+            }
+        });
+
     }
+
+
+
+
+
 }
